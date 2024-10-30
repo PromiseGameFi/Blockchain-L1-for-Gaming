5bd2cdd90887e5c8e41cc942ffe007bfe3468966
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2024-10-30 02:03:42 +0100

    Update key-utils.py

--- CompressionKey/src/key-utils.py
+++ CompressionKey/src/key-utils.py
@@ -9,7 +9,6 @@ def private_key_to_wif(private_key, compressed=False):
     Converts a private key to Wallet Import Format (WIF).
     """
     prefix = b'\x80'  # Prefix for mainnet
-    keyGen= prefix +private_key
     extended_key = prefix + private_key + (b'\x01' if compressed else b'')
     checksum = hashlib.sha256(hashlib.sha256(extended_key).digest()).digest()[:4]
     return base58.b58encode(extended_key + checksum).decode()
