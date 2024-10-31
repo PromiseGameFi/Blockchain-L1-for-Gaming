1b49605053288c5c1d463a43b8ed1886b68e59e5
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2024-10-31 09:29:13 +0100

    main

---
+++ CompressionKey/src/bip38seed/SeedGen/Seedtoseed.py
@@ -0,0 +1,3 @@
+"""Use a predefined set of words for generating the new seed phrase. 
+Encrypt the new seed phrase, which can be decrypted back to retrieve the original seed phrase using the password."""
+
