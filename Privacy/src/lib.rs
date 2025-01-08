954ba6ec388b3d6f9a2658ea2dbe44b0e47ea7ce
Promise Emmanuel <95595405+PromiseGameFi@users.noreply.github.com>
2025-01-08 23:23:28 +0100

    Update lib.rs

--- Privacy/src/lib.rs
+++ Privacy/src/lib.rs
@@ -26,16 +26,7 @@ struct SignatureApp {
 
 impl Default for SignatureApp {
     fn default() -> Self {
-        Self {
-            users: HashMap::new(),
-            current_user: None,
-            recipient: String::new(),
-            message: String::new(),
-            encrypted_messages: Vec::new(),
-            decrypted_messages: Vec::new(),
-            new_username: String::new(),
-            new_userCount: String::new(),
-        }
+       
     }
 }
 
